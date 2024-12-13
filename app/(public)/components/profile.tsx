77090dd3049c3f9c26e19f6df328db439f4ca73e
huszkacs <huszkacs@gmail.com>
2024-12-13 15:34:49 +0800

    Updates profile image

--- app/(public)/components/profile.tsx
+++ app/(public)/components/profile.tsx
@@ -10,7 +10,7 @@ const Profile = (props: ProfileProperites) => {
     return (
         <Image
             alt="Picture of Csaba Zsolt Huszka"
-            className={classNames("rounded-3xl h-32 w-32 lg:h-36 lg:w-36 xl:h-44 xl:w-44", props.className)}
+            className={classNames("rounded-3xl h-32 w-32 lg:h-36 lg:w-36 xl:h-44 xl:w-44 object-cover", props.className)}
             src={profile}
         ></Image>
     )

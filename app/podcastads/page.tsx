eb95657d06934426d3bc08e65400b77129c3aa3e
huszkacs <huszkacs@gmail.com>
2025-01-12 00:04:09 +0800

    pod description

--- app/podcastads/page.tsx
+++ app/podcastads/page.tsx
@@ -7,7 +7,7 @@ const PodcastAdsPage = () => {
         <>
             <header className={classNames("mx-auto max-w-7xl px-6 pb-8 sm:pt-8 xl:pt-16")}
             >
-                <h1 className="text-left text-5xl font-medium pb-4 sm:text-6xl sm:text-center sm:mb-12 xl:text-7xl">
+                <h1 className="text-left text-neutral-800 text-5xl font-medium pb-4 sm:text-6xl sm:text-center sm:mb-12 xl:text-7xl">
                     Érd el a vásárlóidat hatékonyan!
                 </h1>
                 <h2 className="text-2xl">
@@ -17,15 +17,33 @@ const PodcastAdsPage = () => {
             </header>
             <Section>
                 <Section.Title>
-                    Hirdess a Follow The Pattern Podcasten keresztül
+                    Hirdess Szórakoztató és Informatív tartalommal
                 </Section.Title>
-                - Youtube hosszú távon a prémium előfizetőket támogatja, ezzel a hagyományos fizetett
+                <p>Youtube hosszú távon a prémium előfizetőket támogatja, ezzel a hagyományos hirdetések háttérbe fognak szorulni.</p>
+                <p>Lineáris TV nézés ideje lejárt, viszont az embereknek ugyanúgy van igénye szórkoztató és informatiív tartalom nézésére.</p>
+            </Section>
+            <Section>
+                <Section.Title>
+                    Hirdetési lehetőségek
+                </Section.Title>
+                <p>Külön álló short videó Youtube és TikTok csatornán</p>
+                <p>Külön álló rövid videó Youtube csatornán</p>
+                <p>Podast epizódba integrálás</p>
+            </Section>
+            <Section>
+                <Section.Title>
+                    Miért a Follow The Pattern Podcast?
+                </Section.Title>
+                <p>Egyik leggyorsabban feljődő Magyar tudományos műsor!</p>
+                <p>2024-ben, több mint 600.000 ezer embert értünk el, csak Youtube-on.</p>
+                <p>Nézőink, több mint 95%-a 25 életkor feletti, azaz felnőttkori saját megélhetéssel rendelkezik.</p>
+                <p>Több platformon is elérhető.</p>
             </Section>
             <Section id="contact">
                 <ContactForm hun={true} defaultMessage={
                     `Kedves Follow The Pattern!
 
-Szeretnék ajánlatot kérni a hirdetéshez!`
+Szeretnék ajánlatot kérni a podcast-ben való hirdetéshez!`
                 } />
             </Section>
         </>
